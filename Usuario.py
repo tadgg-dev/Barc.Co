41b88a0c5c858e4fd71f1f64da8362f8edf56fce
tadgg-dev <149004649+tadgg-dev@users.noreply.github.com>
2024-06-05 20:03:53 -0300

    Usuario.py

---
+++ Usuario.py
@@ -0,0 +1,19 @@
+class Usuario:
+
+    def __init__(self, nombre, tipo_empleado, usuario, contra):
+        self.nombre = nombre
+        self.apellido = tipo_empleado
+        self.email = usuario
+        self.telefono = contra
+
+    def create_user(nombre,tipo_empleado,usuario,contra):
+        pass
+
+    def find_user(id):
+        pass
+
+    def edit_usuer(id,payload):
+        pass
+    
+    def delete_user(id):
+        pass
